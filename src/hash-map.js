2ccd896fc8c42d77358bbb1097c88e1a48b4c032
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-01-03 18:36:11 +1100

    Add hash to createHashMap

--- src/hash-map.js
+++ src/hash-map.js
@@ -1,4 +1,15 @@
 function createHashMap() {
-  let loadFactor = 0.75;
+  const LOAD_FACTOR = 0.75;
   let capacity = 16;
+
+  function hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
+    }
+
+    return hashCode;
+  }
 }
